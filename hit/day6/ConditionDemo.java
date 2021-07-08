166ba7cdc92b0d8173006c5e790ee39438214067
curiehit <87109452+curiehit@users.noreply.github.com>
2021-07-08 11:37:32 +0530

    Add files via upload

--- hit/day6/ConditionDemo.java
+++ hit/day6/ConditionDemo.java
@@ -11,15 +11,27 @@ public class ConditionDemo {
 		int x=scan.nextInt();
 		
 		if(x%2==0) {
-			System.out.println("Even Number..........");
+			System.out.println("if-else-if -  Even Number..........");
 		}
 		else {
-			System.out.println("Odd Number................");
+			System.out.println("if-else-if - Odd Number................");
 		}
 		
 		//the same thing can also be achieved through TERNARY operator - ?(ternary)
 		
-		System.out.println((x%2==0)?"Even Number...":"Odd Number......");
+		System.out.println((x%2==0)?"Ternary-Even Number...":"Ternary-Odd Number......");
+		
+		switch(x%2) 
+		{
+			case 0:{
+				System.out.println("Switch Case - Even number....");
+				break;
+			}
+			case 1:{
+				System.out.println("Switch Case - odd number....");
+				break;
+			}
+		}
 		
 	}
 }
