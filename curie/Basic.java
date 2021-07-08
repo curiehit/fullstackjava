3e4b3442e8ab20d0ff8ab4cbfe25bbd006144f14
curiehit <87109452+curiehit@users.noreply.github.com>
2021-07-08 09:55:36 +0530

    Add files via upload

---
+++ curie/Basic.java
@@ -0,0 +1,20 @@
+package curie;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		
+		//my input data
+		int i=100;
+		int j=200;
+		
+		//processing
+		
+		int s=i+j;
+		
+		//output
+		System.out.println(s);
+
+	}
+
+}
